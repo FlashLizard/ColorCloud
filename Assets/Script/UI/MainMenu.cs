3dc7e366e03436b2739ec3a19688365f5fab2b37
ctcticxh <3574736597@qq.com>
2023-04-24 21:15:22 +0800

    0

---
+++ Assets/Script/UI/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+
+    }
+    public void Tutorial()
+    {
+
+    }
+    public void Settings()
+    {
+
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
